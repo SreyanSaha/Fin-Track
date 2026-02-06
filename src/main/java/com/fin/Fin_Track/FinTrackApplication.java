c3e782d07c5e0e1e06c9c285f197670e50f03eec
Sreyan Saha <156419805+SreyanSaha@users.noreply.github.com>
2026-02-06 16:10:31 +0530

    basic project made

--- src/main/java/com/fin/Fin_Track/FinTrackApplication.java
+++ src/main/java/com/fin/Fin_Track/FinTrackApplication.java
@@ -1,9 +1,15 @@
 package com.fin.Fin_Track;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fin")
+@EntityScan("com.fin.model")
+@EnableAutoConfiguration
+@EnableJpaRepositories("com.fin.repository")
 public class FinTrackApplication {
 
 	public static void main(String[] args) {
