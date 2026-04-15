38e693b17b27409a2abe24556d5b64c7436c7088
Sreyan Saha <156419805+SreyanSaha@users.noreply.github.com>
2026-04-16 00:41:47 +0530

    Update CacheService.java

--- src/main/java/com/fin/service/CacheService.java
+++ src/main/java/com/fin/service/CacheService.java
@@ -1,53 +1,8 @@
 package com.fin.service;
 
-import com.fin.dto.MonthlyReportPublicDto;
-import com.fin.dto.YearlyReportPublicDto;
-import com.fin.model.User;
 import org.springframework.stereotype.Service;
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class CacheService {
-    private final ConcurrentHashMap<Integer, CacheObject> cacheData=new ConcurrentHashMap<>();
 
-    public void setCacheData(User user, YearlyReportPublicDto yearlyReportPublicDto, MonthlyReportPublicDto monthlyReportPublicDto){
-        cacheData.put(user.getUserId(), new CacheObject(yearlyReportPublicDto, monthlyReportPublicDto));
-    }
-    public ConcurrentHashMap<Integer, CacheObject> getCacheData() {
-        return cacheData;
-    }
-    public void updateCacheData(User user, YearlyReportPublicDto yearlyReportPublicDto, MonthlyReportPublicDto monthlyReportPublicDto){
-        
-    }
-
-
-
-
-
-
-    private class CacheObject{
-        private HashMap<Long, YearlyRecords> yearRecordsHashMap;
-        CacheObject(YearlyReportPublicDto yearlyReportPublicDto, MonthlyReportPublicDto monthlyReportPublicDto){
-
-        }
-
-
-
-
-
-        private class YearlyRecords{
-
-
-
-            YearlyRecords(YearlyReportPublicDto yearlyReportPublicDto, MonthlyReportPublicDto monthlyReportPublicDto){
-
-            }
-
-            private class MonthlyRecords{
-
-            }
-        }
-
-    }
 }
